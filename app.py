9bf436390f47b2d15e25e70cb1e8b8487e1137a7
Cleber Eder Pacheco <cleberederpacheco@gmail.com>
2021-11-23 20:51:31 -0300

    Ajuste Final

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
-from db import db_connection
+import mysql.connector
 
 app = Flask('__name__')
+
+def connect_to_db():
+    db_connection = mysql.connector.connect(host='us-cdbr-east-04.cleardb.com',
+                                            user='b3f5faf814bd93',
+                                            password='1f0b1ae4',
+                                            database='heroku_54813b828b4da3c')
+    return db_connection    
 
 @app.route('/')
 def index():
@@ -12,13 +19,24 @@ def questoes():
     dificuldade = request.args.get('dificuldade')
     modulo = request.args.get('modulo')
 
-    cursor = db_connection.cursor()
+    cnx = connect_to_db();
+    cursor = cnx.cursor()
     
     sql = f"SELECT * FROM questoesdb WHERE dificuldade = '{dificuldade}' and modulo = '{modulo}'"
     cursor.execute(sql)
-    rows = cursor.fetchall()
+    row = cursor.fetchone()
+    questoes = []
 
-    return render_template('questoes.html', rows=rows)
+    while row:
+        enunciado = row[3]
+        gabarito = row[4]
+
+        print(enunciado)
+
+        questoes.append((enunciado, gabarito))
+        row = cursor.fetchone()
+
+    return render_template('questoes.html', questoes=questoes, dificuldade=dificuldade, modulo=modulo )
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)
+    app.run('0.0.0.0', 5000)
